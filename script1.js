05efc254d0a5811847f6dfb31be33dfa9cccfcfc
Aneesh Mohanan <anmh265@gmail.com>
2022-12-23 09:35:30 +0530

    array examples

--- script1.js
+++ script1.js
@@ -1821,13 +1821,13 @@ const euroToUsd = 1.1
 // })
 const movementsUsd = movements.map(mov => mov * euroToUsd)
 
-console.log(movements)
-console.log(movementsUsd)
+// console.log(movements)
+// console.log(movementsUsd)
 
 const movementsUsdfor = []
 for(const mov of movements)
 movementsUsdfor.push(mov * euroToUsd)
-console.log(movementsUsdfor)
+// console.log(movementsUsdfor)
 
 const movementsDescription = movements.map((mov, i, arr) =>
 
@@ -1894,7 +1894,234 @@ const movementsDescription = movements.map((mov, i, arr) =>
 // console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]))
 
 //find()
-const firstWithdrawal = movements.find(mov => mov < 0)
+// const firstWithdrawal = movements.find(mov => mov < 0)
 
-console.log(movements)
-console.log(firstWithdrawal)
+// console.log(movements)
+// console.log(firstWithdrawal)
+
+//some()
+const anyDeposits = movements.some(mov => mov > 0)
+// console.log(anyDeposits)
+
+//every()
+
+// console.log(movements.every(mov => mov > 0))
+
+// const deposit = mov => mov > 0
+// console.log(movements.some(deposit))
+// console.log(movements.every(deposit))
+// console.log(movements.filter(deposit))
+
+//flat()
+// const arr = [[1, 2, 3], [4, 5, 6, 7]]
+// console.log(arr.flat())
+
+// const arrDeep = [[[1 , 2], 3], [4, 5, 6], 7, 8]
+// console.log(arrDeep.flat(2))
+
+//Sorting
+//fot Strings
+const owners = ['Jonas', 'Zach', 'Adam', 'Martha']
+// console.log(owners)
+// console.log(owners.sort())
+
+//for Numbers
+// console.log(movements)
+
+//ascending
+// movements.sort((a, b) => {
+//   if(a > b) return 1
+//   if(a < b) return -1
+// })
+
+movements.sort((a, b) => a - b)
+// console.log(movements)
+
+//descending
+// movements.sort((a, b) => {
+//   if(a > b) return -1
+//   if(a < b) return 1
+// })
+
+movements.sort((a, b) => b - a)
+// console.log(movements)
+
+// const arr = [1, 2, 3, 4, 5, 6]
+// const x = new Array(7)
+// console.log(x)
+// console.log(x.map(() => 5))
+
+// x.fill(1, 3, 5)
+// console.log(x)
+
+// arr.fill(23, 2, 6)
+// console.log(arr)
+
+// //Array.from
+// const y = Array.from({length: 7}, () => 1)
+// console.log(y)
+
+// const z = Array.from({length: 7}, (curr, i) => i + 1)
+// console.log(z)
+
+// const randomDice = Array.from({length: 100}, (curr, i) => Math.floor(Math.random() * 6) + 1)
+// console.log(randomDice)
+
+//Coding challenge
+// const dogs = [
+//   { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+//   { weight: 8, curFood: 200, owners: ['Matilda'] },
+//   { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+//   { weight: 32, curFood: 340, owners: ['Michael'] },
+//   ];
+
+//   dogs.forEach(dog => dog.recFood = Math.trunc(dog.weight ** 0.75 * 28))
+//   console.log(dogs)
+
+
+//  const dogSarah =  dogs.find(dog => dog.owners.includes('Sarah'))
+//  console.log(dogSarah)
+//  console.log(`Sarah's dog is eating too ${dogSarah.curFood > dogSarah.recFood ? 'much': 'little'}`)
+ 
+//  const ownersEatTooMuch = dogs.filter(dog => dog.curFood > dog.recFood).flatMap(dog => dog.owners)
+//  console.log(ownersEatTooMuch)
+
+//  const ownersEatTooLittle = dogs.filter(dog => dog.curFood < dog.recFood).flatMap(dog => dog.owners)
+//  console.log(ownersEatTooLittle)
+
+//  console.log(`${ownersEatTooMuch.join(' and ')}'s dog eat too much!`)
+//  console.log(`${ownersEatTooLittle.join(' and ')}'s dog eat too litlle!`)
+
+//  console.log(dogs.some(dog => dog.curFood === dog.recFood))
+
+// //  console.log(dogs.some(dog => dog.curFood > dog.recFood * 0.9 && dog.curFood < dog.recFood * 1.1))
+// const checkEatingOkay = dog => dog.curFood > dog.recFood * 0.9 && dog.curFood < dog.recFood * 1.1
+
+// console.log(dogs.some(checkEatingOkay))
+
+// console.log(dogs.filter(checkEatingOkay))
+
+// const dogsCopy = dogs.slice().sort((a, b) => a.recFood - b.recFood)
+// console.log(dogsCopy)
+
+// console.log(0.1 + 0.2)
+// console.log(0.1 + 0.2 === 0.3)
+
+// console.log(Number.parseInt('30px', 10))
+// console.log(Number.parseFloat('2.5rem'))
+
+// //NaN
+// // console.log(Number.isNaN(20))
+// // console.log(Number.isNaN('20'))
+// // console.log(Number.isNaN(+'23X'))
+// console.log(Number.isNaN(23 / 0))
+
+
+// // console.log(Number.isFinite(20))
+// // console.log(Number.isFinite('20'))
+// console.log(Number.isFinite(23 / 0))
+
+// console.log(Number.isInteger(23 / 0))
+
+// console.log(Math.sqrt(25))
+// console.log(25 ** (1 / 2))
+// console.log(8 ** (1 / 3))
+
+// console.log(Math.max(5, 18, 11, 2))
+// console.log(Math.max(4, 8, '23X', 6))
+// console.log(Math.max(4, 8, '23', 6))
+// console.log(Math.min(4, 3, 5, 1, 6))
+
+// console.log(Math.PI * Number.parseFloat('10px') ** 2)
+// console.log(Math.trunc(Math.random() * 6 + 1))
+
+
+//GET NUMBERS WITHIN A RANGE
+// const randomInt = (min, max) => Math.floor(Math.random() * (max - min) + 1) + min
+// console.log(randomInt(2, 8))
+
+//Rounding numbers
+// console.log(Math.round(23.6))
+// console.log(Math.round(23.2))
+// console.log(Math.round(-23.2))
+// console.log(Math.round(-23.6))
+
+// console.log(Math.trunc(23.3))
+// console.log(Math.trunc(23.6))
+
+// console.log(Math.ceil(23.6))
+// console.log(Math.ceil(23.2))
+
+// console.log(Math.floor(23.4))
+// console.log(Math.floor(23.6))
+
+// console.log(Math.trunc(-23.4))
+// console.log(Math.floor(-23.4))
+
+// //Rounding decimals
+// console.log((2.4).toFixed(0))
+// console.log((2.4).toFixed(3))
+// console.log(+(2.345).toFixed(2))
+
+// console.log( 5 % 2)
+
+// const isEven = n => n % 2 === 0
+// console.log(isEven(8))
+// console.log(isEven(9))
+
+const diameter = 287_460_00_00
+// console.log(diameter)
+
+const transferFee1 = 15_00
+const transferFee2 = 1_500
+// console.log(transferFee1, transferFee2)
+
+// const PI = 3._145 //Illegal
+// // console.log(PI)
+
+// console.log(Number('230_00'))
+// console.log(parseInt('230_00'))
+
+// console.log(Number.MAX_SAFE_INTEGER)
+// console.log(2 ** 53 - 1)
+
+// console.log(452279664484466514448734n)  //n - BigInt
+// console.log(BigInt(452279664484466514448734))
+
+// console.log(2044587336n + 23) //error
+// console.log(24442n > 200)
+
+//DATE()
+
+// const now = new Date()
+// console.log(now)
+
+const future = new Date(2037, 10, 19, 15, 23)
+// console.log(future)
+// console.log(future.getFullYear())
+// console.log(future.getMonth())
+// console.log(future.getDate())
+// console.log(future.getDay())
+// console.log(future.getHours())
+// console.log(future.getMinutes())
+// console.log(future.getSeconds())
+// console.log(future.toISOString())
+// console.log(future.getTime())
+
+// future.setFullYear(2040)
+// console.log(future)
+
+
+function increment(x){
+  if(x>0){
+    
+    increment(x-1)
+    console.log(x)
+  }
+  else return 
+}
+increment(3)
+
+// const calcDaysPassed = (date1, date2) => Math.abs((date2 - date1) / (1000 * 60 * 60 * 24))
+// const days1 = calcDaysPassed(new Date(2037, 3, 14), new Date(2037, 3, 24))
+// console.log(days1)
